c1302b8c37e145b7770f7849a66c06571638b2c1
Stephen Brennan <smb196@case.edu>
2015-10-22 14:07:15 -0400

    Capitalization.

--- doc/conf.py
+++ doc/conf.py
@@ -109,7 +109,7 @@ pygments_style = 'sphinx'
 todo_include_todos = True
 
 
-# -- Options for HTML output ----------------------------------------------
+# -- Options for HTML Output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
