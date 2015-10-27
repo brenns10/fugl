792a1b2b44fb27cc2b001200f47f016773d492b4
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-10-27 18:10:35 -0400

    Added login_url to settings

--- corvid/corvid/settings.py
+++ corvid/corvid/settings.py
@@ -33,6 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'login/'
 
 # Application definition
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 )
 
 MIDDLEWARE_CLASSES = (
