d319ae6c69fa0ba28f1d15594db0e78255ebb42a
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-10-28 13:55:29 -0400

    Added initial population command for main

---
+++ corvid/main/management/commands/populate.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from main.models.user import User
+from main.models.theme import Theme
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Populate database with default objects'
+
+    def handle(self, *args, **kwargs):
+        admin_user = User.objects.create_user('admin_user',
+                                              'admin@example.com',
+                                              'cock-of-the-rock')
+        admin_user.save()
+
+        default_theme = Theme.objects.create(title='default',
+                                             filepath='themes/default',
+                                             body_markup='<script>console.log("")</script>',
+                                             creator=admin_user)
+        default_theme.save()
