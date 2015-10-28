1d033e6b275d064c6da026b6cb5a06b0f3afa998
Kyle Deal <kdeal@kyledeal.com>
2015-10-27 22:08:57 -0400

    Added templates for every page

--- corvid/main/urls.py
+++ corvid/main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', root_controller),
     url(r'^login', login_view),
     url(r'^home', user_home_controller),
+    url(r'^create_account', login_view, name='create_account'),
 ]
