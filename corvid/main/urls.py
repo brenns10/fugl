fccae0fecc2ba776de919dfdfceb5635c8d42721
Kyle Deal <kdeal@kyledeal.com>
2015-10-27 23:35:19 -0400

    Using register instead of create_account

--- corvid/main/urls.py
+++ corvid/main/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', root_controller),
     url(r'^login', login_view),
     url(r'^home', user_home_controller),
-    url(r'^create_account', login_view, name='create_account'),
+    url(r'^register', login_view, name='register'),
 ]
