99891cecf2e838e02b05f3781cd0a4ca4135fd93
Jeffrey Copeland Jr <jeffrey.copeland.jr@mgail.com>
2015-10-28 10:04:59 -0400

    Switch to django forms for login, registration

--- corvid/main/urls.py
+++ corvid/main/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views.root import root_controller
 from .views.user import user_home_controller
+from .views.register import RegistrationView
 from django.contrib.auth.views import login as login_view
 
 
@@ -8,5 +9,5 @@ urlpatterns = [
     url(r'^$', root_controller),
     url(r'^login', login_view),
     url(r'^home', user_home_controller),
-    url(r'^register', login_view, name='register'),
+    url(r'^register', RegistrationView.as_view()),
 ]
