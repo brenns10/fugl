f2a4b296df7ab4b527e8c0ae8f8d11cd0106057e
Stephen Brennan <smb196@case.edu>
2015-11-08 18:55:30 -0500

    Add a couple tests for the registration view.

---
+++ corvid/main/tests/test_registration_view.py
@@ -0,0 +1,48 @@
+"""
+Tests for main.views.registration.RegistrationView.
+"""
+
+from django.test import Client
+from main.tests.base import CorvidTestCase
+from main.models.user import User
+
+
+class RegistrationViewTestCase(CorvidTestCase):
+
+    def setUp(self):
+        super().setUpTheme()
+        self.client = Client()
+
+    def tearDown(self):
+        super().tearDownTheme()
+
+    def test_get_not_logged_in_returns_form(self):
+        response = self.client.get('/register/')
+        # We should get a 200, and it should contain the form.
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_creates_user(self):
+        post_data = {'username': 'test_user', 'password': 'test_pass',
+                     'email': 'example@example.com'}
+        response = self.client.post('/register/', post_data)
+        # We should get a 200 OK:
+        self.assertEqual(response.status_code, 200)
+        # That contains "Registration successful"
+        self.assertIn('Registration successful', response.content)
+        # After this, we should have a test_user in the database.
+        users = User.objects.filter(username='test_user')
+        self.assertEqual(len(users), 1)
+        # Finally, we can delete the user we created.
+        users[0].delete()
+
+    def test_post_invalid_email_fails(self):
+        post_data = {'username': 'test_user', 'password': 'test_pass',
+                     'email': 'I am not really an email address!'}
+        response = self.client.post('/register/', post_data)
+        # We should get a 200 OK (even though the registration fails)
+        self.assertEqual(response.status_code, 200)
+        # It should contain a message about an invalid email address.
+        self.assertIn('Enter a valid email address', response.content)
+        # After this, we should NOT have a test_user in the database.
+        users = User.objects.filter(username='test_user')
+        self.assertEqual(len(users), 0)
