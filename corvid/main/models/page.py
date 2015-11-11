e8574d0d7a4d8877dac519816444b3325d9838d9
Kyle Deal <kdeal@kyledeal.com>
2015-11-10 21:31:17 -0500

    Added max_length to TextField

--- corvid/main/models/page.py
+++ corvid/main/models/page.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Page(models.Model):
     title = models.CharField(max_length=50)
-    content = models.TextField()
+    content = models.TextField(max_length=50000)
 
     post_plugins = models.ManyToManyField('PagePlugin')
     project = models.ForeignKey('Project')
