e55b6cc4caa042cdfbb4d14aa713cd6345ae52d3
Stephen Brennan <smb196@case.edu>
2015-11-12 15:51:05 -0500

    Add imports in corvid/main/(forms|models|views)/__init__.py.

---
+++ corvid/main/views/__init__.py
@@ -0,0 +1,9 @@
+from .category import CreateCategoryView
+from .create_project import CreateProjectView
+from .page import CreatePageView, UpdatePageView
+from .post import CreatePostView, UpdatePostView
+from .project_home import ProjectDetailView
+from .protected_view import ProtectedViewMixin
+from .registration import RegistrationView
+from .root import root_controller
+from .user import UserHomeView
