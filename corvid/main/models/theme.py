21ce22a6191ca73f52879649295d491d4c560f73
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-11-12 20:39:34 -0500

    Enforce uniqueness on Theme

--- corvid/main/models/theme.py
+++ corvid/main/models/theme.py
@@ -3,7 +3,7 @@ from .user import User
 
 
 class Theme(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     filepath = models.FilePathField()
     body_markup = models.CharField(max_length=5000)
 
