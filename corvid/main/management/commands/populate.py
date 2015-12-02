089039b4c95267ff8627716a9cdeb0fe30811b56
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-12-02 00:29:53 -0500

    merged load themes and populate script

--- corvid/main/management/commands/populate.py
+++ corvid/main/management/commands/populate.py
@@ -1,4 +1,6 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from os.path import isdir, abspath, join
+from os import listdir
 from main.models import User, Theme
 
 
@@ -6,8 +8,13 @@ class Command(BaseCommand):
     args = ''
     help = 'Populate database with default objects'
 
+    def add_arguments(self, parser):
+        parser.add_argument('theme_dir', type=str,
+                            help='directory containing themes')
+
     def handle(self, *args, **kwargs):
-        admin_user = User.objects.create_user('admin_user',
+        ADMIN_USERNAME = 'admin_user'
+        admin_user = User.objects.create_user(ADMIN_USERNAME,
                                               'admin@example.com',
                                               'cock-of-the-rock')
         admin_user.is_superuser = True
@@ -15,8 +22,23 @@ class Command(BaseCommand):
         admin_user.is_active = True
         admin_user.save()
 
-        default_theme = Theme.objects.create(title='default',
-                                             filepath='notmyidea',
-                                             body_markup='<script>console.log("")</script>',
-                                             creator=admin_user)
-        default_theme.save()
+        theme_dir = kwargs['theme_dir']
+
+        # Make sure it's a valid directory.
+        if not isdir(theme_dir):
+            raise CommandError('Theme directory does not exist!')
+
+        theme_path = abspath(theme_dir)
+
+        # Create a name -> fullpath dict of themes.
+        themes = {}
+        for d in listdir(theme_path):
+            full = join(theme_path, d)
+            if isdir(full):
+                themes[d] = full  # TODO: come up with better names than d
+
+        for name, path in themes.items():
+            print('Registering theme %s.' % name)
+            theme = Theme.objects.create(title=name, filepath=path,
+                                         creator=admin_user)
+            theme.save()
--- themes/README.md
+++ themes/README.md
@@ -1,3 +1,5 @@
 All themes herein are from the [pelican-themes](https://github.com/getpelican/pelican-themes)
 directory, but have been modified as-needed to support Plugins for Corvid websites. Licenses
 and READMEs remain the same from the time of cloning.
+
+Only exception: notmyidea -> default.
