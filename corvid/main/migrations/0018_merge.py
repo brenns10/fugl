4fe366dbc327553588b943c560a8f4ee814afee8
Kyle Deal <kdeal@kyledeal.com>
2015-12-03 01:43:30 -0500

    Added merged migration

---
+++ corvid/main/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0017_auto_20151202_1513'),
+        ('main', '0017_auto_20151202_2212'),
+    ]
+
+    operations = [
+    ]
