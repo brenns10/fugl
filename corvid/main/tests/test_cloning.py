b0bafb28e59977b607d3b5f6c031875080ddd62a
Stephen Brennan <smb196@case.edu>
2015-12-03 11:47:31 -0500

    Adjust cloning tests: use itertools to generate test cases, and add post with no category.

--- corvid/main/tests/test_cloning.py
+++ corvid/main/tests/test_cloning.py
@@ -1,6 +1,15 @@
 """
 Tests for cloning projects.
+
+This test module works in a bit of a "special" way.  Cloning has 32 possible
+combinations of inputs (just for which parts of the project to clone).  Rather
+than explicitly write tests for all of them, I made this class which has
+assertions based on the inputs you gave to clone().  It has no actual tests in
+it.  At the bottom, I populate the class with a test case for every possible
+combination of inputs, without having to write them all!
 """
+
+import itertools
 
 from django.utils import timezone
 
@@ -80,223 +89,118 @@ class CloneProjectTestCase(CorvidTestCase):
 
         self.post_two = Post.objects.create(
             title='Second Post', content='I\'m a post!', project=self.project,
-            category=self.category_two, date_created=timezone.now(),
-            date_updated=timezone.now()
+            date_created=timezone.now(), date_updated=timezone.now()
         )
         self.post_one.save()
 
     def tearDown(self):
         super().tearDownTheme()
 
-    def test_clone_nothing(self):
-        new = self.project.clone('cloned', False, False, False, False)
-        # Assert that attributes were cloned properly.
+    def assert_attributes_cloned(self, new):
         self.assertEqual(new.title, 'cloned')
         self.assertEqual(new.description, self.project.description)
         self.assertEqual(new.preview_url, self.project.preview_url)
         self.assertEqual(new.owner, self.project.owner)
 
-        # Categories are cloned
+    def assert_categories_cloned(self, new):
         self.assertEqual(new.category_set.count(), 2)
         self.assertEqual(new.category_set.filter(title=self.category_one.title).count(), 1)
         self.assertEqual(new.category_set.filter(title=self.category_two.title).count(), 1)
 
-        # Assert that the theme was not cloned.
-        self.assertNotEqual(new.theme, self.project.theme)
-        self.assertEqual(new.theme, self.default_theme)
+    def assert_theme(self, new, clone_theme):
+        if clone_theme:
+            self.assertEqual(new.theme, self.project.theme)
+        else:
+            self.assertNotEqual(new.theme, self.project.theme)
+            self.assertEqual(new.theme, self.default_theme)
 
-        # Assert that the pages, posts, and plugins were not cloned.
-        self.assertEqual(new.page_set.count(), 0)
-        self.assertEqual(new.post_set.count(), 0)
-        self.assertEqual(new.pageplugin_set.count(), 0)
-        self.assertEqual(new.projectplugin_set.count(), 0)
+    def assert_pages(self, new, clone_pages, clone_plugins):
+        if clone_pages:
+            self.assertEqual(new.page_set.count(), 2)
+            p1 = new.page_set.get(title=self.page_one.title)
+            self.assertEqual(p1.title, self.page_one.title)
+            self.assertEqual(p1.content, self.page_one.content)
+            self.assertEqual(p1.project, new)
+            p2 = new.page_set.get(title=self.page_two.title)
+            self.assertEqual(p2.title, self.page_two.title)
+            self.assertEqual(p2.content, self.page_two.content)
+            self.assertEqual(p2.project, new)
 
-    def test_clone_theme(self):
-        new = self.project.clone('cloned', True, False, False, False)
-        # Assert that attributes were cloned properly.
-        self.assertEqual(new.title, 'cloned')
-        self.assertEqual(new.description, self.project.description)
-        self.assertEqual(new.preview_url, self.project.preview_url)
-        self.assertEqual(new.owner, self.project.owner)
+            if clone_plugins:
+                self.assertEqual(p1.post_plugins.count(), self.page_one.post_plugins.count())
+                self.assertEqual(p2.post_plugins.count(), self.page_two.post_plugins.count())
+            else:
+                self.assertEqual(p1.post_plugins.count(), 0)
+                self.assertEqual(p2.post_plugins.count(), 0)
+        else:
+            self.assertEqual(new.page_set.count(), 0)
 
-        # Categories are cloned
-        self.assertEqual(new.category_set.count(), 2)
-        self.assertEqual(new.category_set.filter(title=self.category_one.title).count(), 1)
-        self.assertEqual(new.category_set.filter(title=self.category_two.title).count(), 1)
+    def assert_posts(self, new, clone_posts, clone_plugins):
+        if clone_posts:
+            self.assertEqual(new.post_set.count(), 2)
+            p1 = new.post_set.get(title=self.post_one.title)
+            self.assertEqual(p1.title, self.post_one.title)
+            self.assertEqual(p1.content, self.post_one.content)
+            self.assertEqual(p1.date_created, self.post_one.date_created)
+            self.assertEqual(p1.date_updated, self.post_one.date_updated)
+            self.assertNotEqual(p1.category, self.post_one.category)
+            self.assertEqual(p1.category.title, self.post_one.category.title)
+            self.assertEqual(p1.project, new)
+            p2 = new.post_set.get(title=self.post_two.title)
+            self.assertEqual(p2.title, self.post_two.title)
+            self.assertEqual(p2.content, self.post_two.content)
+            self.assertEqual(p2.date_created, self.post_two.date_created)
+            self.assertEqual(p2.date_updated, self.post_two.date_updated)
+            self.assertEqual(p2.category, self.post_two.category)  # category=None
+            self.assertEqual(p2.project, new)
 
-        # Assert that the theme was cloned.
-        self.assertEqual(new.theme, self.project.theme)
+            if clone_plugins:
+                self.assertEqual(p1.post_plugins.count(), self.post_one.post_plugins.count())
+                self.assertEqual(p2.post_plugins.count(), self.post_two.post_plugins.count())
+            else:
+                self.assertEqual(p1.post_plugins.count(), 0)
+                self.assertEqual(p2.post_plugins.count(), 0)
 
-        # Assert that the pages, posts, and plugins were not cloned.
-        self.assertEqual(new.page_set.count(), 0)
-        self.assertEqual(new.post_set.count(), 0)
-        self.assertEqual(new.pageplugin_set.count(), 0)
-        self.assertEqual(new.projectplugin_set.count(), 0)
+        else:
+            self.assertEqual(new.post_set.count(), 0)
 
-    def test_clone_pages(self):
-        new = self.project.clone('cloned', False, True, False, False)
-        # Assert that attributes were cloned properly.
-        self.assertEqual(new.title, 'cloned')
-        self.assertEqual(new.description, self.project.description)
-        self.assertEqual(new.preview_url, self.project.preview_url)
-        self.assertEqual(new.owner, self.project.owner)
+    def assert_plugins(self, new, clone_plugins):
+        if clone_plugins:
+            self.assertEqual(new.pageplugin_set.count(), 1)
+            p = new.pageplugin_set.get(title=self.page_plugin.title)
+            self.assertEqual(p.head_markup, self.page_plugin.head_markup)
+            self.assertEqual(p.body_markup, self.page_plugin.body_markup)
+            self.assertEqual(p.project, new)
+            self.assertEqual(new.projectplugin_set.count(), 1)
+            p = new.projectplugin_set.get(title=self.project_plugin.title)
+            self.assertEqual(p.markup, self.project_plugin.markup)
+            self.assertEqual(p.project, new)
+        else:
+            self.assertEqual(new.pageplugin_set.count(), 0)
+            self.assertEqual(new.projectplugin_set.count(), 0)
 
-        # Categories are cloned
-        self.assertEqual(new.category_set.count(), 2)
-        self.assertEqual(new.category_set.filter(title=self.category_one.title).count(), 1)
-        self.assertEqual(new.category_set.filter(title=self.category_two.title).count(), 1)
 
-        # Assert that the theme was not cloned.
-        self.assertNotEqual(new.theme, self.project.theme)
-        self.assertEqual(new.theme, self.default_theme)
+# Dynamically generate a test for every possible combination of inputs for
+# theme, pages, posts, plugins!
+for theme, pages, posts, plugins in itertools.product(*[[False, True]]*4):
 
-        # Assert that the posts and plugins were not cloned.
-        self.assertEqual(new.post_set.count(), 0)
-        self.assertEqual(new.pageplugin_set.count(), 0)
-        self.assertEqual(new.projectplugin_set.count(), 0)
+    def test(self):
+        new = self.project.clone('cloned', theme, pages, posts, plugins)
+        self.assert_attributes_cloned(new)
+        self.assert_categories_cloned(new)
+        self.assert_theme(new, theme)
+        self.assert_pages(new, pages, plugins)
+        self.assert_posts(new, posts, plugins)
+        self.assert_plugins(new, plugins)
 
-        # Assert that the pages were cloned.
-        self.assertEqual(new.page_set.count(), 2)
-        p1 = new.page_set.get(title=self.page_one.title)
-        self.assertEqual(p1.title, self.page_one.title)
-        self.assertEqual(p1.content, self.page_one.content)
-        self.assertEqual(p1.project, new)
-        p2 = new.page_set.get(title=self.page_two.title)
-        self.assertEqual(p2.title, self.page_two.title)
-        self.assertEqual(p2.content, self.page_two.content)
-        self.assertEqual(p2.project, new)
+    name = 'test_clone'
+    if theme:
+        name += '_theme'
+    if pages:
+        name += '_pages'
+    if posts:
+        name += '_posts'
+    if plugins:
+        name += '_plugins'
 
-        # Since the plugins were not cloned, p1 and p2 will not have any!
-        self.assertEqual(p1.post_plugins.count(), 0)
-        self.assertEqual(p2.post_plugins.count(), 0)
-
-    def test_clone_posts(self):
-        new = self.project.clone('cloned', False, False, True, False)
-        # Assert that attributes were cloned properly.
-        self.assertEqual(new.title, 'cloned')
-        self.assertEqual(new.description, self.project.description)
-        self.assertEqual(new.preview_url, self.project.preview_url)
-        self.assertEqual(new.owner, self.project.owner)
-
-        # Assert that the theme was not cloned.
-        self.assertNotEqual(new.theme, self.project.theme)
-        self.assertEqual(new.theme, self.default_theme)
-
-        # Assert that the pages and plugins were not cloned.
-        self.assertEqual(new.page_set.count(), 0)
-        self.assertEqual(new.pageplugin_set.count(), 0)
-        self.assertEqual(new.projectplugin_set.count(), 0)
-
-        # Assert that the posts were cloned.
-        self.assertEqual(new.post_set.count(), 2)
-        p1 = new.post_set.get(title=self.post_one.title)
-        self.assertEqual(p1.title, self.post_one.title)
-        self.assertEqual(p1.content, self.post_one.content)
-        self.assertEqual(p1.date_created, self.post_one.date_created)
-        self.assertEqual(p1.date_updated, self.post_one.date_updated)
-        self.assertNotEqual(p1.category, self.post_one.category)
-        self.assertEqual(p1.category.title, self.post_one.category.title)
-        self.assertEqual(p1.project, new)
-        p2 = new.post_set.get(title=self.post_two.title)
-        self.assertEqual(p2.title, self.post_two.title)
-        self.assertEqual(p2.content, self.post_two.content)
-        self.assertEqual(p2.date_created, self.post_two.date_created)
-        self.assertEqual(p2.date_updated, self.post_two.date_updated)
-        self.assertNotEqual(p2.category, self.post_two.category)
-        self.assertEqual(p2.category.title, self.post_two.category.title)
-        self.assertEqual(p2.project, new)
-
-        # Since plugins weren't cloned, none of them have plugins.
-        self.assertEqual(p1.post_plugins.count(), 0)
-        self.assertEqual(p2.post_plugins.count(), 0)
-
-    def test_clone_plugins(self):
-        new = self.project.clone('cloned', False, False, False, True)
-        # Assert that attributes were cloned properly.
-        self.assertEqual(new.title, 'cloned')
-        self.assertEqual(new.description, self.project.description)
-        self.assertEqual(new.preview_url, self.project.preview_url)
-        self.assertEqual(new.owner, self.project.owner)
-
-        # Categories are cloned
-        self.assertEqual(new.category_set.count(), 2)
-        self.assertEqual(new.category_set.filter(title=self.category_one.title).count(), 1)
-        self.assertEqual(new.category_set.filter(title=self.category_two.title).count(), 1)
-
-        # Assert that the theme was not cloned.
-        self.assertNotEqual(new.theme, self.project.theme)
-        self.assertEqual(new.theme, self.default_theme)
-
-        # Assert that the pages and posts were not cloned.
-        self.assertEqual(new.page_set.count(), 0)
-        self.assertEqual(new.post_set.count(), 0)
-
-        # Assert that plugins are cloned.
-        self.assertEqual(new.pageplugin_set.count(), 1)
-        p = new.pageplugin_set.get(title=self.page_plugin.title)
-        self.assertEqual(p.head_markup, self.page_plugin.head_markup)
-        self.assertEqual(p.body_markup, self.page_plugin.body_markup)
-        self.assertEqual(p.project, new)
-        self.assertEqual(new.projectplugin_set.count(), 1)
-        p = new.projectplugin_set.get(title=self.project_plugin.title)
-        self.assertEqual(p.markup, self.project_plugin.markup)
-        self.assertEqual(p.project, new)
-
-    def test_clone_all(self):
-        new = self.project.clone('cloned', True, True, True, True)
-        # Assert that attributes were cloned properly.
-        self.assertEqual(new.title, 'cloned')
-        self.assertEqual(new.description, self.project.description)
-        self.assertEqual(new.preview_url, self.project.preview_url)
-        self.assertEqual(new.owner, self.project.owner)
-
-        # Assert that the theme was cloned.
-        self.assertEqual(new.theme, self.project.theme)
-
-        # Assert that plugins are cloned.
-        self.assertEqual(new.pageplugin_set.count(), 1)
-        p = new.pageplugin_set.get(title=self.page_plugin.title)
-        self.assertEqual(p.head_markup, self.page_plugin.head_markup)
-        self.assertEqual(p.body_markup, self.page_plugin.body_markup)
-        self.assertEqual(p.project, new)
-        self.assertEqual(new.projectplugin_set.count(), 1)
-        p = new.projectplugin_set.get(title=self.project_plugin.title)
-        self.assertEqual(p.markup, self.project_plugin.markup)
-        self.assertEqual(p.project, new)
-
-        # Assert that the pages were cloned.
-        self.assertEqual(new.page_set.count(), 2)
-        p1 = new.page_set.get(title=self.page_one.title)
-        self.assertEqual(p1.title, self.page_one.title)
-        self.assertEqual(p1.content, self.page_one.content)
-        self.assertEqual(p1.project, new)
-        p2 = new.page_set.get(title=self.page_two.title)
-        self.assertEqual(p2.title, self.page_two.title)
-        self.assertEqual(p2.content, self.page_two.content)
-        self.assertEqual(p2.project, new)
-        # Since the plugins were cloned, they will have the same amount
-        self.assertEqual(p1.post_plugins.count(), self.page_one.post_plugins.count())
-        self.assertEqual(p2.post_plugins.count(), self.page_two.post_plugins.count())
-
-        # Assert that the posts were cloned.
-        self.assertEqual(new.post_set.count(), 2)
-        p1 = new.post_set.get(title=self.post_one.title)
-        self.assertEqual(p1.title, self.post_one.title)
-        self.assertEqual(p1.content, self.post_one.content)
-        self.assertEqual(p1.date_created, self.post_one.date_created)
-        self.assertEqual(p1.date_updated, self.post_one.date_updated)
-        self.assertNotEqual(p1.category, self.post_one.category)
-        self.assertEqual(p1.category.title, self.post_one.category.title)
-        self.assertEqual(p1.project, new)
-        p2 = new.post_set.get(title=self.post_two.title)
-        self.assertEqual(p2.title, self.post_two.title)
-        self.assertEqual(p2.content, self.post_two.content)
-        self.assertEqual(p2.date_created, self.post_two.date_created)
-        self.assertEqual(p2.date_updated, self.post_two.date_updated)
-        self.assertNotEqual(p2.category, self.post_two.category)
-        self.assertEqual(p2.category.title, self.post_two.category.title)
-        self.assertEqual(p2.project, new)
-        # Since plugins were cloned, they will have the same amount.
-        self.assertEqual(p1.post_plugins.count(), self.post_one.post_plugins.count())
-        self.assertEqual(p2.post_plugins.count(), self.post_two.post_plugins.count())
-
+    setattr(CloneProjectTestCase, name, test)
